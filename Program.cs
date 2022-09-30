856f308e4a42b927d71288a9183ed4710017277b
Vitor <vfarias@bionexo.com>
2022-09-30 16:48:58 -0400

    add

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Teste.estatica = "estatica nova";
+// Teste.constante = "constante nova";
+
+var teste = new Teste("apenasLeitura nova");
+// teste.apenasLeitura = "apenas leitura nova";
+
+Console.WriteLine(Teste.estatica);
+Console.WriteLine(Teste.constante);
+Console.WriteLine(teste.apenasLeitura);
+
+public class Teste
+{
+    public Teste(string texto)
+    {
+        apenasLeitura = texto;
+    }
+    public static string estatica = "estatica";
+    public const string constante = "constante nao pode alterar";
+    public readonly string apenasLeitura = "apenasLeitura";
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Const vs Readonly
+
+`const` é uma "estática" (está vinculada a classe e não a instancia) que não pode ser alterada. Seu valor é definido em tempo de compilação.
+
+`readonly` é uma "variavel" (está vinculada à instância do objeto) que não pode ser alterada. Seu valor é definido em tempo de execução (construtor).
